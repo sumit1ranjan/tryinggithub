9462d686d4cdb3f83e22c69775d9bcbdc04e3b47
sumit ranjan <kixo@gmail.com>
2026-08-06 00:45:42 +0530

    first project

---
+++ index.js
@@ -0,0 +1,8 @@
+// Display a message in the browser console
+console.log("JavaScript is connected!");
+
+// Change the heading text
+document.querySelector("h1").textContent = "Welcome to My Website!";
+
+// Change the paragraph text
+document.querySelector("p").textContent = "This content was updated using JavaScript.";
